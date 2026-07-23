83e77cd887bf7e11d0486c6fe68f03a5119b86e3
nareshnaik14 <nareshrathod277779@gmail.com>
2026-07-23 13:25:18 +0530

    Correct dataset loading logic in placement.py
    Fix indentation and syntax error in dataset loading section.

--- placement.py
+++ placement.py
@@ -109,7 +109,7 @@ Features Used:
 # -------------------------------------------------------
 # LOAD DATASET
 # -------------------------------------------------------
-  df=None
+  
 if student_dataset is not None
      df=load_data(student_dataset)
 
