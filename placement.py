7da934ee66b435c90e8e26014e13feb1d4393859
nareshnaik14 <nareshrathod277779@gmail.com>
2026-07-23 10:02:26 +0530

    Update placement.py

--- placement.py
+++ placement.py
@@ -70,12 +70,12 @@ st.sidebar.markdown("---")
 
 student_dataset = st.sidebar.file_uploader(
     "Upload Student Dataset",
-    type=["csv","xlsx", "xls", "json", "txt", "tsv"]
+    type=["csv","xlsx", "xls", "json", "txt", "tsv","pdf"]
 )
 
 single_student = st.sidebar.file_uploader(
     "Upload Single Student File",
-    type=["csv","xlsx", "xls", "json", "txt", "tsv"]
+    type=["csv","xlsx", "xls", "json", "txt", "tsv","pdf"]
 )
 
 st.sidebar.markdown("---")
