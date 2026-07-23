d90bb7e209bf85a351deef5666b67a631f0629ee
nareshnaik14 <nareshrathod277779@gmail.com>
2026-07-24 01:02:37 +0530

    Fix indentation in placement.py for pie chart

--- placement.py
+++ placement.py
@@ -359,7 +359,7 @@ elif page == "📊 Data Analysis":
             st.header("📈 Student Prediction Analytics")
     #result = st.session_state["prediction_df"]
             col1, col2 = st.columns(2)
-        with col1:
+    with col1:
             fig = px.pie(
                 result,
            names="Predicted_Placement",
