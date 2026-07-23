ed46f5b47352a947f208220952b7acba18997198
nareshnaik14 <nareshrathod277779@gmail.com>
2026-07-23 16:45:02 +0530

    Update placement.py

--- placement.py
+++ placement.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+import joblib
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
